e1df9085011a0db3a6736013914e189fbead353b
Ricardo <ricardoespinosa400@gmail.com>
2021-08-03 21:03:19 -0500

    se avanzo en la estructura del portafolio

---
+++ Portafolio/descripcion.php
@@ -0,0 +1,31 @@
+<style>
+    h1{
+        color: white;
+        font-size:57px;
+    }
+    h2{
+        color: white;
+        font-size:41px;
+    }
+    .Caja_descripcion{    
+    margin: 0 auto;
+    width: 90%;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    aling-content: center;
+    }
+    .descripcion{
+        height:10px;
+    }
+    
+</style>
+<section class="Caja_descripcion">
+    <div class="descripcion">
+<h1>Hola a todos, Soy Ricardo <img src="img/👋.png" alt=""> </h1>
+<h2>Soy un apasionado de la programacion, <br>con experiencia en el desarrollo web </h1>
+    </div>
+    <div class="descripcion">
+<img src="img/Grupo 20.png" alt="">        
+    </div>
+</section>
---
+++ Portafolio/menu.php
@@ -0,0 +1,23 @@
+<header>
+<input type="checkbox" id="btn-menu">
+ <label for="btn-menu"><img src="" alt=""> </label>
+ 
+
+<nav class="menu">
+    <ul>
+        <li>Educacion</li>
+        <li>Inicio</li>
+        <li class="logo"> <img src="img/Ardilla logo Cabecera.png" alt=""> </li>
+        <li>Habilidades</li>
+        <li>Contacto</li>
+        <li class="menu-seccion2"><img src="img/github logo cabecera.png" alt=""></li>
+        <li class="menu-seccion2"><img src="img/linkedin logo cabecera.png" alt=""></li>
+        <li class="menu-seccion2"><img src="img/youtube logo cabecera.png" alt=""></li>
+
+    </ul>
+
+ 
+    
+    
+</nav>     
+</Header>
