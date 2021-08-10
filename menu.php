0c39c5ce0a5fb71dde89d6435467a286be24bd58
Ricardo <ricardoespinosa400@gmail.com>
2021-08-10 16:12:36 -0500

    se agrego el index en .html para visualizar el portafolio en github pages

--- menu.php
+++ menu.php
@@ -1,7 +1,11 @@
 <header>
-<input type="checkbox" id="btn-menu">
- <label for="btn-menu"><img src="" alt=""> </label>
- 
+<input type="checkbox" id="btn-menu"> 
+
+<div class="flex-container">
+  <div class="c1"><label for="btn-menu"><img src="img/menu.png" alt=""> </label></div>
+  <div class="c2"><img src="img/Ardilla logo Cabecera movil.png" alt=""></div>
+
+</div>
 
 <nav class="menu">
     <ul>
@@ -10,9 +14,11 @@
         <li class="logo"> <img src="img/Ardilla logo Cabecera.png" alt=""> </li>
         <li>Habilidades</li>
         <li>Contacto</li>
+        
         <li class="menu-seccion2"><img src="img/github logo cabecera.png" alt=""></li>
         <li class="menu-seccion2"><img src="img/linkedin logo cabecera.png" alt=""></li>
         <li class="menu-seccion2"><img src="img/youtube logo cabecera.png" alt=""></li>
+
 
     </ul>
 
---
+++ proyectos.php
@@ -0,0 +1,11 @@
+<center>
+<h1 class="subtitulo">Proyectos</h1>
+<div class="p-1">
+    <div class="caja-p-1"><img  class="img-p"src="img/gran bunker.png" alt=""></div>
+    <div class="text-p-1">
+        <h1>Gran Bunker</h1>
+        <h2 class="fuente1">Es un proyecto personal , su giro de la empresa es comercial dedicada a la venta<br>
+ de productos como cafeteras , molinos , insumos, refacciones y mas.</h2>
+</div>
+</div>
+</center>
