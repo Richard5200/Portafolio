a8710f948f6f3ecfc10c29a560b8b915960de6d5
Ricardo <ricardoespinosa400@gmail.com>
2021-08-12 20:29:45 -0500

    se agregaron proyectos

--- proyectos.php
+++ proyectos.php
@@ -15,11 +15,11 @@
 </div>
 
 <div class="tarjeta_p">
-    <div><img class="imagen_p" src="img/Web 1920 – 1.png" alt=""></div>
+    <div><img class="imagen_p" src="img/seguridad neza logo.png" alt=""></div>
     <div class="texto_p">
-        <h4>Proyecto Gran Bunker</h4>
-        <p>Este es un proyecto personal, el cual es una tienda online dedicada a la venta de articulos como cafeteras,capsulas , cafe de grano .etc </p>
-        <img class="img-tecnologias-p" src="img/tecnologias p1.png" alt=""><br>
+        <h4>Policia de nezahualcoyotl</h4>
+        <p>Servi como apoyo en el desarrollo de diferentes plataformas web durante un año</p>
+        <img class="img-tecnologias-p" src="img/p2 tecnologias.png" alt=""><br>
         <a href="#">Leer mas</a>
 
 
@@ -27,29 +27,14 @@
 </div>
 
 <div class="tarjeta_p">
-    <div><img class="imagen_p" src="img/Web 1920 – 1.png" alt=""></div>
+    <div><img class="imagen_p" src="img/4905784.jpg" alt=""></div>
     <div class="texto_p">
-        <h4>Proyecto Gran Bunker</h4>
-        <p>Este es un proyecto personal, el cual es una tienda online dedicada a la venta de articulos como cafeteras,capsulas , cafe de grano .etc </p>
-        <img class="img-tecnologias-p" src="img/tecnologias p1.png" alt=""><br>
+        <h4>Instructor de plataformas de comunicacion</h4>
+        <p>Di dos cursos sobre plataformas de comunicacion para docentes de educacion media superior</p>
         <a href="#">Leer mas</a>
-
-
     </div>
 </div>
 
-<div class="tarjeta_p">
-    <div><img class="imagen_p" src="img/Web 1920 – 1.png" alt=""></div>
-    <div class="texto_p">
-        <h4>Proyecto Gran Bunker</h4>
-        <p>Este es un proyecto personal, el cual es una tienda online dedicada a la venta de articulos como cafeteras,capsulas , cafe de grano .etc </p>
-        <img class="img-tecnologias-p" src="img/tecnologias p1.png" alt=""><br>
-        <a href="#">Leer mas</a>
-
-
-    </div>
 </div>
 
-
-</div>
 </Center>
